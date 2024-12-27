092ce9f6aa7cabcaa27ab0668fbbe4d51d381c31
Andrew Zambazos <andrewzambazos@gmail.com>
2024-12-27 22:10:55 +1300

    enemy added only follow

---
+++ new-game-project/Scenes/Objects/enemy.gd
@@ -0,0 +1,23 @@
+extends CharacterBody2D
+
+
+@export var speed = 25
+var player_chase = false
+var player = null
+
+func _physics_process(delta: float) -> void:
+	if player_chase: 
+		position += (player.position - position) / speed
+
+
+
+
+
+func _on_detection_area_body_entered(body: Node2D) -> void:
+	player = body
+	player_chase = true
+
+
+func _on_detection_area_body_exited(body: Node2D) -> void:
+	player = null
+	player_chase = false
