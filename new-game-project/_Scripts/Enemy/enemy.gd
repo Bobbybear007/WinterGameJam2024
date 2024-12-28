ee96a39cf6624894f721ae6ec9890af9124ca963
Beetruth <108978219+BeetruthDev@users.noreply.github.com>
2024-12-28 17:46:12 +1300

    Guard only chases if player has item

--- new-game-project/_Scripts/Enemy/enemy.gd
+++ new-game-project/_Scripts/Enemy/enemy.gd
@@ -1,38 +1,33 @@
 extends CharacterBody2D
 
-@export var sus_time = 6
-@export var short_sus_time = 2
+@export var sus_time = 2
 @export var speed = 25
 
 @onready var animated_sprite = $AnimatedSprite
 
 var player_chase = false
+var player_in_hit_range = false
+var player_has_item = false
 var player = null
 
 func _on_detection_area_body_entered(body: Node2D) -> void:
 	if body is CharacterBody2D:
 		player = body
-
 		
-		if player.has_pickup_item():
-			sus_time = short_sus_time
+		player_has_item = player.has_pickup_item()
+		if player_has_item:
 			print("Player has item. Using short sus time:", sus_time)
+			call_deferred("start_chase")
 		else:
-			sus_time = 6
-			print("Player does NOT have item. Using regular sus time:", sus_time)
-
-		call_deferred("start_chase")
+			print("Yeah no this weirdo looks perfectly normal, no weird bulges or anything")
 
 func _on_max_agro_range_body_exited(body: Node2D) -> void:
 	if body is CharacterBody2D:
+		print("<color=green>exited range</color>")
 		player = null
 		player_chase = false
 
 func start_chase() -> void:
-	if player != null:
-		
-		sus_time = short_sus_time if player.has_pickup_item() else 6
-
 	var timer = get_tree().create_timer(sus_time)
 	await timer.timeout
 
@@ -41,6 +36,8 @@ func start_chase() -> void:
 
 func _process(delta: float) -> void:
 	handle_animation()
+	if player_chase and player_in_hit_range:
+		call_deferred("_change_scene")
 
 func _physics_process(delta: float) -> void:
 	if player_chase and player:
@@ -57,8 +54,10 @@ func handle_animation() -> void:
 
 func _on_hit_area_body_entered(body):
 	if body.name == "Player":
-		call_deferred("_change_scene")
-
+		player_in_hit_range = true
+	
+func _on_hit_area_body_exited(body: Node2D) -> void:
+	player_in_hit_range = false
 
 func _change_scene():
 	get_tree().change_scene_to_file("res://Scenes/Game/kill_map.tscn")
