9e1ce71a6eb46da505106e1a0b8b0c6c1b06d255
Andrew Zambazos <andrewzambazos@gmail.com>
2024-12-29 22:04:05 +1300

    drop / pickup hat added
    - fan hide hat
    - hat no spawn beside player yet...
    - if other hat in scene player can pick that one up

--- new-game-project/_Scripts/Player/Player.gd
+++ new-game-project/_Scripts/Player/Player.gd
@@ -10,69 +10,80 @@ extends CharacterBody2D
 var has_pickup = false
 var direction = 1
 var input = Vector2()
+var has_hat = false  # Default is no hat until picked up
 
 func _ready():
-	var noodles = $"../Noodles"
-	var screwdriver = $"../Screwdriver"
-	var toilet_paper = $"../ToiletPaper"
+    # Connect the hat pickup signal
+    var hat_pickup = $"../HatPickup"  # Adjust to your hat's node path
+    if hat_pickup:
+        print("Connecting hat_picked_up signal to the Player")
+        hat_pickup.connect("hat_picked_up", Callable(self, "hat_picked_up"))
+    else:
+        print("Hat pickup node not found!")
 
-	if noodles:
-		add_pickup_object(noodles)
-	if screwdriver:
-		add_pickup_object(screwdriver)
-	if toilet_paper:
-		add_pickup_object(toilet_paper)
+    # Connect other pickup objects (optional, example)
+    var noodles = $"../Noodles"
+    var screwdriver = $"../Screwdriver"
+    var toilet_paper = $"../ToiletPaper"
+
+    if noodles:
+        add_pickup_object(noodles)
+    if screwdriver:
+        add_pickup_object(screwdriver)
+    if toilet_paper:
+        add_pickup_object(toilet_paper)
 
 func add_pickup_object(pickup_object):
-	print("Connecting pickup object signal to the Player")
-	pickup_object.connect("picked_up", Callable(self, "_on_pickup_object_picked_up"))
+    print("Connecting pickup object signal to the Player")
+    pickup_object.connect("picked_up", Callable(self, "_on_pickup_object_picked_up"))
 
 func _on_pickup_object_picked_up() -> void:
-	print("Pickup object collected!")
-	has_pickup = true
-	print("has_pickup is now:", has_pickup)
+    print("Pickup object collected!")
+    has_pickup = true
+    print("has_pickup is now:", has_pickup)
 
 func has_pickup_item() -> bool:
-	print("has_pickup_item() called, returning:", has_pickup)
-	return has_pickup
-
-func get_input() -> void:
-	input = Input.get_vector("Left", "Right", "Up", "Down")
-	velocity = input * speed
-
-func _physics_process(delta: float) -> void:
-	get_input()
-	move_and_slide()
-
-func _process(delta: float) -> void:
-	handle_animation()
-	handle_flip()
-	pass
-
-func handle_animation() -> void:
-	if input.x == 0 and input.y == 0:
-		play_animation("idle")
-	else:
-		play_animation("move")
-
-func play_animation(name):
-	base_animation_player.play(name)
-	hat_animation_player.play(name)
-	jacket_animation_player.play(name)
-
-func handle_flip() -> void:
-	if input.x == 0:
-		return
-
-	if round(input.x) != direction:
-		direction *= -1
-		visuals.scale.x = direction
-
-
-func drop_hat() -> void:
-	hat_animation_player.visible = false  # Hides the hat sprite
+    print("has_pickup_item() called, returning:", has_pickup)
+    return has_pickup
 
 func hat_picked_up() -> void:
-	print("Hat pickup signal received!")
-	hat_animation_player.visible = true
-	print("New hat visibility:", hat_animation_player.visible)
+    print("Hat pickup signal received!")
+    hat_animation_player.visible = true
+    print("Hat visibility is now:", hat_animation_player.visible)
+
+
+func get_input() -> void:
+    input = Input.get_vector("Left", "Right", "Up", "Down")
+    velocity = input * speed
+
+func _physics_process(delta: float) -> void:
+    get_input()
+    move_and_slide()
+
+func _process(delta: float) -> void:
+    handle_animation()
+    handle_flip()
+
+func handle_animation() -> void:
+    if input.x == 0 and input.y == 0:
+        play_animation("idle")
+    else:
+        play_animation("move")
+
+func play_animation(name: String) -> void:
+    base_animation_player.play(name)
+    hat_animation_player.play(name)
+    jacket_animation_player.play(name)
+
+func handle_flip() -> void:
+    if input.x == 0:
+        return
+
+    if round(input.x) != direction:
+        direction *= -1
+        visuals.scale.x = direction
+
+func drop_hat() -> void:
+    # Hides the hat sprite
+    hat_animation_player.visible = false
+    has_hat = false
--- new-game-project/_Scripts/hat_pickup.gd
+++ new-game-project/_Scripts/hat_pickup.gd
@@ -2,21 +2,12 @@ extends Area2D
 
 signal hat_picked_up
 
-func _ready():
-	
-	print("Children of this node:", get_children())
+func _ready() -> void:
+    connect("body_entered", Callable(self, "_on_body_entered"))
 
-	
-	var pickup_area = get_node("Pickup_area")
-	if pickup_area:
-		
-		pickup_area.connect("body_entered", Callable(self, "_on_body_entered"))
-	else:
-		print("Pickup_area node not found")
-
-func _on_body_entered(body: Node2D) -> void:
-	
-	if body.name == "Player" and body.has_method("drop_hat"):
-		print("Player entered pickup area")
-		emit_signal("hat_picked_up")
-		queue_free()
+func _on_body_entered(body: Node) -> void:
+    if body is CharacterBody2D:
+        print("Player entered pickup area")
+        emit_signal("hat_picked_up")
+        print("Signal emitted: hat_picked_up")
+        queue_free()
