1f75eae6c10a885fe3fa3deefe94375fc4834375
Beetruth <108978219+BeetruthDev@users.noreply.github.com>
2024-12-30 18:11:16 +1300

    tileset 2.0

--- new-game-project/_Scripts/Pickup.gd
+++ new-game-project/_Scripts/Pickup.gd
@@ -3,13 +3,13 @@ extends Area2D
 signal picked_up
 
 func _ready() -> void:
-    
-    if not is_connected("body_entered", Callable(self, "_on_body_entered")):
-        connect("body_entered", Callable(self, "_on_body_entered"))
+	
+	if not is_connected("body_entered", Callable(self, "_on_body_entered")):
+		connect("body_entered", Callable(self, "_on_body_entered"))
 
 func _on_body_entered(body):
-    if body is CharacterBody2D:
-        print("Player entered pickup area")
-        
-        emit_signal("picked_up")
-        queue_free()
+	if body is CharacterBody2D:
+		print("Player entered pickup area")
+		
+		emit_signal("picked_up")
+		queue_free()
